345fa0473dee27172e519d98a97af86ea5552057
Joseph Robinson <smartech56@gmail.com>
2023-12-22 15:14:14 -0700

    Updated Example project with better code conventions

--- BlazorDB.Example/Program.cs
+++ BlazorDB.Example/Program.cs
@@ -13,33 +13,33 @@ builder.Services.AddBlazorDB(options =>
 {
     options.Name = "Test";
     options.Version = 1;
-    options.StoreSchemas = new List<StoreSchema>
-    {
-        new StoreSchema()
+    options.StoreSchemas =
+    [
+        new StoreSchema
         {
             Name = "Person",
             PrimaryKey = "id",
             PrimaryKeyAuto = true,
-            UniqueIndexes = new List<string> { "guid" },
-            Indexes = new List<string> { "name" }
+            UniqueIndexes = ["guid"],
+            Indexes = ["name"]
         }
-    };
+    ];
 });
 builder.Services.AddBlazorDB(options =>
 {
     options.Name = "Test2";
     options.Version = 1;
-    options.StoreSchemas = new List<StoreSchema>()
-    {
-        new StoreSchema()
+    options.StoreSchemas =
+    [
+        new StoreSchema
         {
             Name = "Item",
             PrimaryKey = "id",
             PrimaryKeyAuto = true,
-            UniqueIndexes = new List<string> { "guid" },
-            Indexes = new List<string> { "name" }
+            UniqueIndexes = ["guid"],
+            Indexes = ["name"]
         }
-    };
+    ];
 });
 
 await builder.Build().RunAsync();
